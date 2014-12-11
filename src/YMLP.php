db8827ca5a2f3b91df795cd4a9df369c1c2ac3fd
Drew Hammond <drew@alphagenetica.com>
2014-12-10 21:26:49 -0500

    Add unit tests and sample ping function

--- src/YMLP.php
+++ src/YMLP.php
@@ -4,10 +4,29 @@ class YMLP
 {
     protected $apiKey;
     protected $username;
+    protected $output;
 
-    public function __construct($apiKey, $username)
+    public function __construct($apiKey, $username, $output = 'xml')
     {
         $this->apiKey = $apiKey;
         $this->username = $username;
+        $this->output = $output;
+    }
+
+    /**
+     * Ping the API server.
+     *
+     * Ping() is the simplest command, doesn't serve any useful purpose but is a great command to
+     * understand this API feature and to test your API implementation.
+     *
+     * When you call this command, it will output "Hello!"
+     *
+     * @link http://www.ymlp.com/app/api_command.php?command=Ping
+     * @todo Under development
+     * @return string
+     */
+    public function ping()
+    {
+        return 'hello';
     }
 }
---
+++ tests/YMLPTest.php
@@ -0,0 +1,31 @@
+<?php
+
+class YMLPTest extends PHPUnit_Framework_TestCase
+{
+
+    protected $ymlp;
+
+    public function __construct()
+    {
+
+        $apiKey = 'abc123';
+        $username = 'dummy';
+        $output = 'xml';
+
+        $this->ymlp = new \Alphagenetica\YMLP\YMLP($apiKey, $username, $output);
+    }
+
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+
+    /**
+     * @test
+     */
+    public function ping_returns_yes_or_no()
+    {
+        $result = $this->ymlp->ping();
+        $this->assertContains('hello', $result);
+    }
+}
